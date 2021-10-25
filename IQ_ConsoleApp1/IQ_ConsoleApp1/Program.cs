5cdf8e58eb16311b48031d8d6686b60e75d8b301
Ishtiaq Qureshi <ishtiaq@gmail.com>
2021-10-25 23:23:49 +0100

    Level 1 modules 1+2
    Codenga Level 1 modules 1 and 2

---
+++ IQ_ConsoleApp1/IQ_ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IQ_ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            //This is a variable
+
+            string message = "This is a message using a variable";
+
+            Console.WriteLine(message);
+
+            //Concatenation
+
+            string prefix = "Mr";
+            string name = "Schweinsteiger";
+
+
+            Console.WriteLine(prefix + name);
+
+            //Length
+
+            Console.WriteLine(name.Length);
+
+
+
+
+        }
+    }
+}
