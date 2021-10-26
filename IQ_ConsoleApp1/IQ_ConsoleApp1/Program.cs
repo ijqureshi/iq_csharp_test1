9fef071a4188fc85b9a647ae5882c01be8de49c8
Ishtiaq Qureshi <ishtiaq@gmail.com>
2021-10-26 23:55:46 +0100

    Arrays
    Arrays section

--- IQ_ConsoleApp1/IQ_ConsoleApp1/Program.cs
+++ IQ_ConsoleApp1/IQ_ConsoleApp1/Program.cs
@@ -27,6 +27,40 @@ namespace IQ_ConsoleApp1
 
             Console.WriteLine(name.Length);
 
+            // Character
+
+            char letter = 'A';
+            Console.WriteLine(letter);
+
+            //Booleans
+
+            bool isAdmin = true;
+            bool isActive = false;
+
+            Console.WriteLine(isAdmin);
+            Console.WriteLine(isActive);
+
+
+            //Arrays
+
+            int[] numbers = new int[4];
+            Console.WriteLine(numbers);
+
+            int[] morenumbers = new int[3];
+            Console.WriteLine(morenumbers[0]);
+            Console.WriteLine(morenumbers[1]);
+            Console.WriteLine(morenumbers[2]);
+
+
+            int[] values = new int[] { 8, 4, 16, 32 };
+            Console.WriteLine(values[0]);
+            Console.WriteLine(values[1]);
+            Console.WriteLine(values[2]);
+            Console.WriteLine(values[3]);
+
+
+
+
 
 
 
